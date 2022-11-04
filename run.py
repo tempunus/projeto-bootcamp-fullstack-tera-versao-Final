3e54a90b8a721b5183ac5cb47df5ff26f8189cb3
dyda91 <edjamasalmeida@outlook.com>
2022-11-04 06:19:13 -0300

    novo commit

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import manager
 
+port = int(os.environ.get('PORT', 5000))
 
 if __name__ == "__main__":
-    manager.run(port="5000")
+    manager.run(host='0.0.0.0', debug=True, port=port)
